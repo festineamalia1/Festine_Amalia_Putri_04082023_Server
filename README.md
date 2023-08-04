3dd7efeaf68b11e29da7b93a8930297692229194
Festine-Capio <festine@capioteknologi.co.id>
2023-08-04 18:14:04 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Festine_Amalia_Putri_04082023_Server
+# Festine Amalia tes 03/08/2023 Server
+
+
+## Available Scripts
+
+In the project directory, you can install:
+### `npm install`
+
+In the project directory, you can run:
+
+### `nodemon`
+
+Runs the app in the development mode.<br />
+
+list api :
+- GET http://localhost:8080/users
+- GET http://localhost:8080/users/{id}
+- POST http://localhost:8080/users
+- POST http://localhost:8080/users/edit/{id}
+- POST http://localhost:8080/users/delete/{id}
+
---
+++ src/controllers/controller-users.js
@@ -0,0 +1,118 @@
+const config = require("../configs/database");
+const mysql = require("mysql");
+const pool = mysql.createPool(config);
+const bycript = require("bcrypt");
+
+const md5 = require("md5");
+
+pool.on("error", (err) => {
+  console.log(err);
+});
+
+module.exports = {
+  getDataUsers(req, res) {
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(`SELECT * FROM user;`, function (error, results) {
+        if (error) throw error;
+        res.send({
+          success: true,
+          message: "Berhasil",
+          data: results,
+        });
+      });
+      connection.release();
+    });
+  },
+  getDataUsersById(req, res) {
+    let id = req.params.id;
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        `SELECT * FROM user WHERE userid = ?;`,
+        [id],
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            success: true,
+            message: "Berhasil",
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
+
+  addDataUser(req, res) {
+    // const hashPassword = bycript.hash(req.body.pass, 10);
+    let data = {
+      namalengkap: req.body.nama,
+      username: req.body.user,
+      password: md5(req.body.pass),
+      status: req.body.status,
+    };
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        `INSERT INTO user SET ?;`,
+        [data],
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            success: true,
+            message: "Berhasil",
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
+
+  editDataUser(req, res) {
+    let id = req.params.id;
+    let dataEdit = {
+      namalengkap: req.body.nama,
+      username: req.body.user,
+      password: req.body.pass,
+      status: req.body.status,
+    };
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        `UPDATE user SET ? WHERE USERID = ? ;`,
+        [dataEdit, id],
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            success: true,
+            message: "Berhasil",
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
+
+  deleteDataUser(req, res) {
+    let id = req.params.id;
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      connection.query(
+        `DELETE FROM user WHERE userid = ?;`,
+        [id],
+        function (error, results) {
+          if (error) throw error;
+          res.send({
+            success: true,
+            message: "Berhasil",
+            data: results,
+          });
+        }
+      );
+      connection.release();
+    });
+  },
+};
