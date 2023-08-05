83d80d1c1643a3bc28a39f367a72c64cdc1cfdad
Festine-Capio <festine@capioteknologi.co.id>
2023-08-05 23:20:00 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(cors());
 
 const appRoute = require('./src/routes/route-users');
 app.use('/', appRoute);
